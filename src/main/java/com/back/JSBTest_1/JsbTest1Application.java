b153b853d348b3dcc8353776fff9413e8776f0f1
gurum505 <sayhojeong505@gmail.com>
2025-12-01 22:09:31 +0900

    mission2

---
+++ src/main/java/com/back/JSBTest_1/JsbTest1Application.java
@@ -0,0 +1,13 @@
+package com.back.JSBTest_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsbTest1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JsbTest1Application.class, args);
+	}
+
+}
