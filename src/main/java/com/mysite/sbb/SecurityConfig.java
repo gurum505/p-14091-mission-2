ccdc516856c784927f1404accece84818d9ff6c1
gurum505 <sayhojeong505@gmail.com>
2025-12-01 18:00:43 +0900

    JSB 3-7 로그인 로그아웃 기능 구현

--- src/main/java/com/mysite/sbb/SecurityConfig.java
+++ src/main/java/com/mysite/sbb/SecurityConfig.java
@@ -1,13 +1,17 @@
 package com.mysite.sbb;
 
+import org.apache.tomcat.util.net.openssl.ciphers.Authentication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.header.writers.frameoptions.XFrameOptionsHeaderWriter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
@@ -15,18 +19,32 @@ public class SecurityConfig {
 
     @Bean
     SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests((ah)->{
-            ah.requestMatchers("/**").permitAll();
-        }).csrf((csrf) -> csrf
-                .ignoringRequestMatchers("/h2-console/**"))
+        http
+                .authorizeHttpRequests((authorizeHttpRequests) -> authorizeHttpRequests
+                        .requestMatchers("/**").permitAll())
+                .csrf((csrf) -> csrf
+                        .ignoringRequestMatchers("/h2-console/**"))
                 .headers((headers) -> headers
                         .addHeaderWriter(new XFrameOptionsHeaderWriter(
-                                XFrameOptionsHeaderWriter.XFrameOptionsMode.SAMEORIGIN)));
+                                XFrameOptionsHeaderWriter.XFrameOptionsMode.SAMEORIGIN)))
+                .formLogin((formLogin) -> formLogin
+                        .loginPage("/user/login")
+                        .defaultSuccessUrl("/"))
+                .logout((logout) -> logout
+                        .logoutUrl("/user/logout")
+                        .logoutSuccessUrl("/")
+                        .invalidateHttpSession(true))
+        ;
         return http.build();
     }
 
     @Bean
     PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
     }
 }
--- src/main/java/com/mysite/sbb/user/UserRepository.java
+++ src/main/java/com/mysite/sbb/user/UserRepository.java
@@ -3,6 +3,9 @@ package com.mysite.sbb.user;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<SiteUser,Integer> {
+    Optional<SiteUser> findByUsername(String username);
 }
